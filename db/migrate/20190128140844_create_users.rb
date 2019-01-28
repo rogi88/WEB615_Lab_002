e5fc239672518637a409b351570a3ba2b86eed2f
Rogi88 <igor.peric777@gmail.com>
2019-01-28 09:26:41 -0500

    Lab2 hopefully done

---
+++ db/migrate/20190128140844_create_users.rb
@@ -0,0 +1,19 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.true :presence
+      t.string :last_name
+      t.true :presence
+      t.string :emial
+      t.true :presence
+      t.string :street_address
+      t.string :city
+      t.string :province
+      t.string :postal_code
+      t.string :country
+
+      t.timestamps
+    end
+  end
+end
