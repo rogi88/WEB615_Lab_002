820a24ba09bbb5c01f11d8a439feec099759479c
Rogi88 <igor.peric777@gmail.com>
2019-01-28 09:45:08 -0500

    The presence has been cleansed, really now

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -69,6 +69,6 @@ class MicropostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def micropost_params
-      params.require(:micropost).permit(:message, :presence, :user_id)
+      params.require(:micropost).permit(:message, :user_id)
     end
 end
